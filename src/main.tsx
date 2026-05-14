f10c5a7f704d648599e6d2b6eef655011c4b4c7d
Alex Passos <alexdesouzapassos@gmail.com>
2026-05-14 15:42:06 -0300

    Integrate Google OAuth in main.tsx
    Wrap App component with GoogleOAuthProvider for OAuth support.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
+import './index.css';
+
+// Usaremos uma variável de ambiente, mas por enquanto você pode deixar a string vazia 
+// ou colocar o seu Client ID caso já tenha um.
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "SEU_CLIENT_ID_AQUI";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
-)
+);
