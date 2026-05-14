9d089b6575759a4d840f94802f0be7ce53703f56
Alex Passos <alexdesouzapassos@gmail.com>
2026-05-14 10:45:02 -0300

    Commit Inicial

---
+++ src/styles/tailwind.config.js
@@ -0,0 +1,12 @@
+theme: {
+  extend: {
+    colors: {
+      brand: {
+        bg: '#F8FAFC',
+        surface: '#FFFFFF',
+        primary: '#4F46E5', // Indigo
+        special: '#8B5CF6', // Violet para os futuros poderes
+      }
+    }
+  }
+}
