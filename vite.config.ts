2936217bdf2ac52e51c95ec9cfac225f5ca608c0
Alex Passos <alexdesouzapassos@gmail.com>
2026-05-14 13:16:24 -0300

    Update Vite config for absolute path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: './', // Garante que ele olhe para a pasta atual
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './index.html',
+        main: resolve(__dirname, 'index.html'), // Força o caminho absoluto do index.html
       },
     },
   },
