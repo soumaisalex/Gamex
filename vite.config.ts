1a3877727294862abada5bf67b4ee95fd8b30d65
Alex Passos <alexdesouzapassos@gmail.com>
2026-05-14 11:54:27 -0300

    Modify Vite config to add rollup input options
    Updated Vite configuration to include rollup input options.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
   },
-  // Isso garante que o Vite procure o index.html na raiz
-  root: './' 
-})
+});
